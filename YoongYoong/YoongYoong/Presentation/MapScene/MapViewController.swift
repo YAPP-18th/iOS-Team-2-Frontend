e4c546fbe1b6bb63121be3b451032319bba8a487
Steve <steve_son1015@naver.com>
2021-04-05 21:05:24 +0900

    feat: 네이버 맵 client key 추가 및 맵뷰 추가

--- YoongYoong/YoongYoong/Presentation/MapScene/MapViewController.swift
+++ YoongYoong/YoongYoong/Presentation/MapScene/MapViewController.swift
@@ -6,11 +6,39 @@
 //
 
 import UIKit
+import NMapsMap
+import SnapKit
 
 class MapViewController: UIViewController {
   
+  var mapView: NMFMapView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    configuration()
+    setupView()
+    setupLayout()
+  }
+}
+
+extension MapViewController {
+  private func configuration() {
+    self.view.backgroundColor = .white
+  }
+  
+  private func setupView() {
+    self.mapView = NMFMapView()
+    self.view.addSubview(self.mapView)
+  }
+  
+  private func setupLayout() {
+    mapView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
+  }
+  
+  private func updateView() {
     
   }
 }
