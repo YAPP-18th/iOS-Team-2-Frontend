259a82b7e6fb093b000f0f6b24e6acbe5ffb43b6
Steve <steve_son1015@naver.com>
2021-04-05 20:43:49 +0900

    feat: Map Scene 추가

---
+++ YoongYoong/YoongYoong/Presentation/MapScene/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  YoongYoong
+//
+//  Created by 손병근 on 2021/04/05.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
