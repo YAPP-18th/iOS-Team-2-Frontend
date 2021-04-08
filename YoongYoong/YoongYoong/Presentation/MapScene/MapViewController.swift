7ead7d337dad44b6f25fca1a82d1312e5b859fde
Steve <steve_son1015@naver.com>
2021-04-09 00:33:27 +0900

    feat: NaverMapView 관련 delegate 연결

--- YoongYoong/YoongYoong/Presentation/MapScene/MapViewController.swift
+++ YoongYoong/YoongYoong/Presentation/MapScene/MapViewController.swift
@@ -25,10 +25,15 @@ class MapViewController: UIViewController {
 extension MapViewController {
   private func configuration() {
     self.view.backgroundColor = .white
+    mapView = NMFMapView()
+    mapView.allowsRotating = false
+    mapView.allowsTilting = false
+    mapView.addCameraDelegate(delegate: self)
+    mapView.touchDelegate = self
   }
   
   private func setupView() {
-    self.mapView = NMFMapView()
+    
     self.view.addSubview(self.mapView)
   }
   
@@ -40,5 +45,21 @@ extension MapViewController {
   
   private func updateView() {
     
+  }
+}
+
+extension MapViewController: NMFMapViewTouchDelegate {
+  private func didTap(_ marker: NMFMarker) {
+    //Todo: 마커 터치시 수행할 작업 ex) 핀 활성화 및 매장 선택
+  }
+  
+  func mapView(_ mapView: NMFMapView, didTapMap latlng: NMGLatLng, point: CGPoint) {
+    //Todo: 맵 터치시 수행할 작업 ex) 핀 비활성화 및 매장 선택 취소
+  }
+}
+
+extension MapViewController: NMFMapViewCameraDelegate {
+  func mapView(_ mapView: NMFMapView, cameraDidChangeByReason reason: Int, animated: Bool) {
+    //Todo: 맵 카메라 이동 완료시 수행할 작업 ex) 현위치 기준 핀 새로고침 등
   }
 }
