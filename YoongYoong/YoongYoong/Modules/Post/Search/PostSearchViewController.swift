30373599ab36fe380216af992f9b601f97d157d9
hyunsik won <shik0601@gmail.com>
2021-04-20 15:26:13 +0900

    feat: 포스트 - 검색 UI 구성

---
+++ YoongYoong/YoongYoong/Modules/Post/Search/PostSearchViewController.swift
@@ -0,0 +1,160 @@
+//
+//  PostSearchViewController.swift
+//  YoongYoong
+//
+//  Created by 원현식 on 2021/04/11.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+import Then
+
+class PostSearchViewController: ViewController {
+  
+  let progressView = UIProgressView()
+  let titleLabel = UILabel()
+  let searchBarContainer = UIView()
+  let searchTextField = UITextField()
+  let searchButton = UIButton()
+  
+  let listContainer = UIView()
+  let searchHistoryView = PostSearchHistoryView()
+  let searchResultView = PostSearchResultView()
+    
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+  override func bindViewModel() {
+    super.bindViewModel()    
+  }
+  
+  override func setupView() {
+    super.setupView()
+    view.addSubview(progressView)
+    view.addSubview(titleLabel)
+    searchBarContainer.addSubview(searchTextField)
+    searchBarContainer.addSubview(searchButton)
+    view.addSubview(searchBarContainer)
+    view.addSubview(listContainer)
+    listContainer.addSubview(searchHistoryView)
+    listContainer.addSubview(searchResultView)
+
+  }
+  
+  override func setupLayout() {
+    super.setupLayout()
+    progressView.snp.makeConstraints { make in
+      make.width.equalTo(view.safeAreaLayoutGuide.snp.width)
+      make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+      make.centerX.equalTo(view)
+    }
+    
+    titleLabel.snp.makeConstraints{ make in
+      make.top.equalTo(progressView.snp.bottom).offset(16)
+      make.left.equalTo(view.snp.left).offset(16)
+    }
+    
+    searchBarContainer.snp.makeConstraints { make in
+      make.width.equalTo(view.snp.width).multipliedBy(0.93)
+      make.top.equalTo(titleLabel.snp.bottom).offset(32)
+      make.height.equalTo(40)
+      make.centerX.equalTo(view)
+    }
+    
+    searchTextField.snp.makeConstraints { make in
+      make.centerY.equalTo(searchBarContainer)
+      make.height.equalTo(searchBarContainer.snp.height).multipliedBy(0.90)
+      make.left.equalTo(searchBarContainer.snp.left).offset(15)
+      make.right.equalTo(searchButton.snp.left)
+    }
+    
+    searchButton.snp.makeConstraints { make in
+      make.height.equalTo(searchTextField.snp.height)
+      make.width.equalTo(searchButton.snp.height)
+      make.centerY.equalTo(searchBarContainer)
+      make.right.equalTo(searchBarContainer).offset(-5)
+    }
+    
+    listContainer.snp.makeConstraints {
+      $0.top.equalTo(searchBarContainer.snp.bottom).offset(3)
+      $0.bottom.left.right.equalTo(view)
+    }
+    
+    searchHistoryView.snp.makeConstraints {
+      $0.edges.equalTo(listContainer)
+    }
+    
+    searchResultView.snp.makeConstraints {
+      $0.edges.equalTo(listContainer)
+    }
+    
+  }
+  
+  override func configuration() {
+    super.configuration()
+    view.backgroundColor = .white
+    progressView.do {
+      $0.progressTintColor = .brandPrimary
+      $0.progress = 0.5
+    }
+    
+    titleLabel.do {
+      $0.text = "어디에서 용기를 냈나요?"
+      $0.font = UIFont.sdGhothicNeo(ofSize: 24, weight: .regular)
+    }
+    
+    searchBarContainer.do {
+      $0.layer.cornerRadius = 12
+      $0.layer.borderColor = #colorLiteral(red: 0.8980392157, green: 0.8980392157, blue: 0.9176470588, alpha: 1).cgColor
+      $0.layer.borderWidth = 1
+    }
+    
+    searchTextField.do {
+      $0.borderStyle = .none
+      $0.delegate = self
+      $0.placeholder = "검색어를 입력하세요"
+      $0.clearButtonMode = .whileEditing
+      $0.font = UIFont.sdGhothicNeo(ofSize: 16, weight: .regular)
+    }
+    
+    searchButton.do {
+      $0.addTarget(self, action: #selector(searchButtonDidTap), for: .touchUpInside)
+    }
+    
+    searchButton.do {
+      $0.setImage(#imageLiteral(resourceName: "searchStroked"), for: .normal)
+    }
+    
+    searchHistoryView.do {
+      $0.isHidden = true
+    }
+    searchResultView.do {
+      $0.isHidden = true
+    }
+
+    
+  }
+  
+  @objc
+  private func searchButtonDidTap() {
+    // TODO: view -> viewModel
+    searchTextField.resignFirstResponder()
+    searchResultView.isHidden = false
+    searchHistoryView.isHidden = true
+  }
+  
+}
+
+
+extension PostSearchViewController: UITextFieldDelegate {
+  func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+    searchResultView.isHidden = true
+    searchHistoryView.isHidden = false
+    
+    return true
+  }
+}
---
+++ YoongYoong/YoongYoong/Modules/Post/Search/PostSearchViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  PostSearchViewModel.swift
+//  YoongYoong
+//
+//  Created by 원현식 on 2021/04/20.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class PostSearchViewModel: ViewModel, ViewModelType {
+  struct Input {
+    // textFieldShouldBeginEditing, searchButtonDidTap, removeButtonDidTap, removeAllButtonDidTap, searchResultItemDidTap
+  }
+  
+  struct Output {
+    // fetchSearchHistory, fetchSearchResult, postMapView
+  }
+  
+  func transform(input: Input) -> Output {
+    return Output()
+  }
+  
+}
