7c2c6c0d2598833e61667b065c7545cd2932918f
Steve <steve_son1015@naver.com>
2021-04-27 11:21:24 +0900

    feat: Feed, TitleContentItem model 추가

---
+++ YoongYoong/YoongYoong/Models/Feed.swift
@@ -0,0 +1,17 @@
+//
+//  Feed.swift
+//  YoongYoong
+//
+//  Created by 손병근 on 2021/04/27.
+//
+
+import Foundation
+
+struct Feed {
+  var nickname: String?
+  var profileImageURL: String?
+  var storeName: String?
+  var date: String?
+  var contentImageURL: String?
+  var menuList: [TitleContentItem]?
+}
