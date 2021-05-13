d1beb9fecee1468efdda5026a3b5f0075b4534d2
hyunsik won <shik0601@gmail.com>
2021-05-13 15:19:34 +0900

    refactor: rx 코드 수정

--- YoongYoong/YoongYoong/Modules/Post/SelectMenu/SelectMenuViewController.swift
+++ YoongYoong/YoongYoong/Modules/Post/SelectMenu/SelectMenuViewController.swift
@@ -23,8 +23,8 @@ class SelectMenuViewController: ViewController {
   private let tableView = UITableView()
   private let nextButton = UIButton().then {
     $0.layer.cornerRadius = 30.0
-    $0.backgroundColor = .brandColorGreen01
-    $0.isEnabled = true
+    $0.backgroundColor = .brandColorGreen03
+    $0.isEnabled = false
     $0.setTitle("계속하기", for: .normal)
     $0.titleLabel?.font = .krButton1
     $0.layer.applySketchShadow(color: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), alpha: 0.12, x: 0, y: 2, blur: 10, spread: 0)
@@ -40,76 +40,73 @@ class SelectMenuViewController: ViewController {
     guard let viewModel = viewModel as? SelectMenuViewModel else { return }
     
     let input = SelectMenuViewModel.Input(tipButtonDidTap: tipButton.rx.tap.asObservable(),
-                                          containerTextFieldDidBeginEditing: BehaviorRelay(value: nil))
+                                          containerTextFieldDidBeginEditing: PublishRelay<Int>(),
+                                          addMenuButtonDidTap: PublishRelay<Void>(),
+                                          removeCell: PublishRelay<Int>(),
+                                          menuCountChanged: PublishSubject<(count: Int,index: Int)>(),
+                                          containerCountChanged: PublishSubject<(count: Int,index: Int)>(),
+                                          menuText: PublishSubject<(txt: String?, index: Int)>())
     
     let output = viewModel.transform(input: input)
     
-    output.info
+    output.menuInfo
       .observeOn(MainScheduler.instance)
       .bind(to: tableView.rx.items(cellIdentifier: MenuInfoCell.reuseIdentifier, cellType: MenuInfoCell.self)) { [weak self] index, info, cell in
         guard let self = self else { return }
-        cell.setSeletedColor()
-        if index == viewModel.menus.count-1 {
-          cell.addMenuButton.isHidden = false
-          cell.headerView.isHidden = true
-          cell.bodyView.isHidden = true
-          
-          cell.addMenuDidTap = {
-            viewModel.addMenu()
-            self.tableView.scrollToRow(at: IndexPath(row: index+1, section: 0), at: .bottom, animated: true)
-          }
+        
+        cell.bind()
+        
+        if info.last == true {
+          cell.setLastCell()
+          cell.addMenuCell
+            .bind { [weak self] in
+              guard let self = self else { return }
+              input.addMenuButtonDidTap.accept(())
+              self.tableView.scrollToRow(at: IndexPath(row: index, section: 0), at: .middle, animated: true)
+            }.disposed(by: self.disposeBag)
+            
         } else {
-          cell.cellTitle.text = "구매 정보\(index+1)"
-          cell.menuTextField.text = info.menu
-          cell.menuCountLabel.text = "\(info.menuCount)"
-          cell.containerTextField.text = info.container
-          cell.containerCountLabel.text = "\(info.containerCount)"
-          
-          cell.containerTextField.rx.controlEvent(.editingDidBegin)
-            .map { _ in return index}
-            .bind { index in
-              cell.containerTextField.resignFirstResponder()
+          cell.setCellData(info, index+1)
+          cell.showContainerListView
+            .bind {
               input.containerTextFieldDidBeginEditing.accept(index)
-              
             }
             .disposed(by: self.disposeBag)
           
-          cell.didDelete = {
-            viewModel.removeCellDidTap(at: index)
-          }
-          cell.menuMinusDidTap = {
-            viewModel.decreaseMenuCount(at: index)
-          }
-          cell.menuPlusDidTap = {
-            viewModel.increaseMenuCount(at: index)
-          }
-          cell.containerMinusDidTap = {
-            viewModel.decreaseContainerCount(at: index)
-          }
-          cell.containerPlusDidTap = {
-            viewModel.increaseContainerCount(at: index)
-          }
+          cell.deleteCell
+            .map {index}
+            .bind(to: input.removeCell)
+            .disposed(by: self.disposeBag)
+
+          cell.menuText
+            .map {($0, index)}
+            .bind(to: input.menuText)
+            .disposed(by: self.disposeBag)
+          
+          cell.menuCount
+            .map { ($0, index) }
+            .bind(to: input.menuCountChanged)
+            .disposed(by: self.disposeBag)
+          
+          cell.containerCount
+            .map { ($0, index) }
+            .bind(to: input.containerCountChanged)
+            .disposed(by: self.disposeBag)
+          
         }
 
- 
       }.disposed(by: disposeBag)
     
     output.buttonEnabled
-      .observeOn(MainScheduler.instance)
-      .subscribe(onNext: { [weak self] flag in
+      .subscribe(onNext: { [weak self] bool in
         guard let self = self else { return }
-//        self.nextButton.isEnabled = flag
-//        self.nextButton.backgroundColor = flag ? .brandColorGreen01 : .brandColorGreen02
+        self.nextButton.isEnabled = bool
+        self.nextButton.backgroundColor = bool ? .brandColorGreen01 : .brandColorGreen03
       }).disposed(by: disposeBag)
     
-    nextButton.rx.tap.bind {
-      self.navigator.show(segue: .addReview(viewModel: PostReviewViewModel()), sender: self, transition: .navigation())
-      
-    }.disposed(by: disposeBag)
-    
     output.containerListView
-      .subscribe(onNext: { viewModel in
-        guard let viewModel = viewModel else { return }
+      .subscribe(onNext: { [weak self] viewModel in
+        guard let self = self else { return }
         self.present(SelectContainerViewController(viewModel: viewModel, navigator: self.navigator), animated: true, completion: nil)
       }).disposed(by: disposeBag)
     
@@ -120,6 +117,10 @@ class SelectMenuViewController: ViewController {
       tipVC.tipView.layer.cornerRadius = 15
       tipVC.dimmView.isHidden = true
       self.present(tipVC, animated: true, completion: nil)
+    }.disposed(by: disposeBag)
+    
+    nextButton.rx.tap.bind {
+      self.navigator.show(segue: .addReview(viewModel: PostReviewViewModel()), sender: self, transition: .navigation())
     }.disposed(by: disposeBag)
     
   }
@@ -167,17 +168,8 @@ class SelectMenuViewController: ViewController {
       $0.separatorStyle = .none
       $0.rowHeight = MenuInfoCell.height
     }
-    
-    tableView.rx.setDelegate(self).disposed(by: disposeBag)
-    
+        
   }
-  
-}
-
-extension SelectMenuViewController: UITableViewDelegate {
-  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-  }
-  
   
 }
 
--- YoongYoong/YoongYoong/Modules/Post/SelectMenu/SelectMenuViewModel.swift
+++ YoongYoong/YoongYoong/Modules/Post/SelectMenu/SelectMenuViewModel.swift
@@ -12,88 +12,94 @@ import RxCocoa
 struct MenuInfo {
   var menu: String?
   var menuCount = 1
-  var container: String?
+  var container: String? = "냄비"
   var containerCount = 1
+  var last: Bool
 }
 
 class SelectMenuViewModel: ViewModel, ViewModelType {
   struct Input {
     let tipButtonDidTap: Observable<Void>
-    var containerTextFieldDidBeginEditing: BehaviorRelay<Int?>
+    let containerTextFieldDidBeginEditing: PublishRelay<Int>
+    let addMenuButtonDidTap: PublishRelay<Void>
+    let removeCell: PublishRelay<Int>
+    let menuCountChanged: PublishSubject<(count: Int, index: Int)>
+    let containerCountChanged: PublishSubject<(count: Int, index: Int)>
+    let menuText: PublishSubject<(txt: String?, index: Int)>
+
   }
   
   struct Output {
-    var info: BehaviorRelay<[MenuInfo]> = BehaviorRelay(value: [])
-    let buttonEnabled: BehaviorRelay<Bool> = BehaviorRelay(value: false)
-    // tip
-    var containerListView: BehaviorRelay<SelectContainerViewModel?> = BehaviorRelay(value: nil)
+    let menuInfo: BehaviorSubject<[MenuInfo]>
+    let buttonEnabled: BehaviorRelay<Bool>
+    let containerListView: PublishRelay<SelectContainerViewModel>
   }
-  var output = Output()
+  
   
   func transform(input: Input) -> Output {
-     input.containerTextFieldDidBeginEditing
-      .skip(1)
-      .map { $0 != nil }
-      .subscribe(onNext: { _ in
-        self.output.containerListView.accept(SelectContainerViewModel())
+    var menus: [MenuInfo] = [.init(last: false), .init(last: true)]
+    let menuInfoOutput = BehaviorSubject<[MenuInfo]>(value: menus)
+    let buttonEnabledOutput = BehaviorRelay<Bool>(value: false)
+    let containerListViewOutput = PublishRelay<SelectContainerViewModel>()
+    
+    input.containerTextFieldDidBeginEditing
+      .bind { index in
+        containerListViewOutput.accept(SelectContainerViewModel())
+      }.disposed(by: disposeBag)
+    
+    
+    input.addMenuButtonDidTap
+      .subscribe(onNext:{
+        menus.insert(.init(last: false), at: menus.count-1)
+        menuInfoOutput.onNext(menus)
       }).disposed(by: disposeBag)
     
-    output.info.accept(menus)
-    
-    output.info
-      .map {self.checkNextButtonValid($0)}
-      .subscribe(onNext: { [weak self] bool in
-        guard let self = self else { return }
-        self.output.buttonEnabled.accept(bool)
+    input.removeCell
+      .subscribe(onNext: { index in
+        guard menus.count > 2 else { return }
+        menus.remove(at: index)
+        menuInfoOutput.onNext(menus)
       }).disposed(by: disposeBag)
     
-    return output
-  }
-  
-  var menus: [MenuInfo] = [.init(), .init()]
-  
-  func addMenu() {
-    menus.insert(.init(), at: menus.count-1)
-    output.info.accept(menus)
-  }
-  
-  func removeCellDidTap(at index: Int) {
-    guard menus.count > 2 else { return }
-    menus.remove(at: index)
-    output.info.accept(menus)
-  }
-  
-  func increaseMenuCount(at index: Int) {
-    guard menus[index].menuCount < 100 else { return }
-    menus[index].menuCount += 1
-    output.info.accept(menus)
-  }
-  
-  func decreaseMenuCount(at index: Int) {
-    guard menus[index].menuCount > 1 else { return }
-    menus[index].menuCount -= 1
-    output.info.accept(menus)
-  }
-  
-  func increaseContainerCount(at index: Int) {
-    guard menus[index].containerCount < 100 else { return }
-    menus[index].containerCount += 1
-    output.info.accept(menus)
-  }
-  
-  func decreaseContainerCount(at index: Int) {
-    guard menus[index].containerCount > 1 else { return }
-    menus[index].containerCount -= 1
-    output.info.accept(menus)
-  }
-  
-  
+    input.menuCountChanged
+      .subscribe(onNext: {
+        menus[$0.index].menuCount += $0.count
+        if menus[$0.index].menuCount == 0 {
+          menus[$0.index].menuCount = 1
+        }
+        menuInfoOutput.onNext(menus)
+      }).disposed(by: disposeBag)
+    
+    input.containerCountChanged
+      .subscribe(onNext: {
+        menus[$0.index].containerCount += $0.count
+        if menus[$0.index].containerCount == 0 {
+          menus[$0.index].containerCount = 1
+        }
+        menuInfoOutput.onNext(menus)
+      }).disposed(by: disposeBag)
 
+    input.menuText
+      .subscribe(onNext: {
+        menus[$0.index].menu = $0.txt
+        menuInfoOutput.onNext(menus)
+      }).disposed(by: disposeBag)
+        
+    menuInfoOutput
+      .map {self.checkNextButtonValid($0)}
+      .bind(to: buttonEnabledOutput)
+      .disposed(by: disposeBag)
+    
+    
+    return Output(menuInfo: menuInfoOutput,
+                  buttonEnabled: buttonEnabledOutput,
+                  containerListView: containerListViewOutput)
+  }
   
   private func checkNextButtonValid(_ datas: [MenuInfo]) -> Bool {
     for i in 0..<datas.count-1 {
       let data = datas[i]
-      if data.container == nil || data.menu == nil {
+      if data.container == nil || data.menu == nil || data.container?.count == 0 || data.menu?.count == 0 {
         return false
       }
     }
