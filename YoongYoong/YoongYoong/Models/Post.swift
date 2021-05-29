c532217310db6a30f0953e39e6aedadaad67f279
hyunsik won <shik0601@gmail.com>
2021-05-29 15:36:44 +0900

    feat: 모델 구성

---
+++ YoongYoong/YoongYoong/Models/Post.swift
@@ -0,0 +1,30 @@
+//
+//  Post.swift
+//  YoongYoong
+//
+//  Created by 원현식 on 2021/05/27.
+//
+
+import Foundation
+
+class PostData {
+  static let shared = PostData()
+  var containers: [PostContainer]?
+  var postImages: [Data]?
+  var reviewBadge: String?
+  var placeName: String?
+  var placeLocation: String?
+  private init() {}
+
+}
+
+struct PostContainer {
+  let container: ContainerInfo
+  let containerCount: Int
+  let food: String
+  let foodCount: Int
+}
+struct ContainerInfo {
+  let name: String
+  let size: String
+}
